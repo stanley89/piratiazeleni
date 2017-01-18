78ac95db5117a67faf129d32221e84b1818e5067
Stanislav Stipl <stanislav.stipl@gmail.com>
2017-01-18 18:54:49 +0100

    zastupitele

---
+++ app/presenters/ZastupitelePresenter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+    App\Model;
+use Nette\Application\UI\Form;
+
+/**
+ * Zastupitele presenter.
+ */
+class ZastupitelePresenter extends BasePresenter
+{
+
+}
